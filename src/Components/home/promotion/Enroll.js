3bba30e50647fd32912625d2681e3a260b7c437f
Matt <boblikesbbq@gmail.com>
2018-12-30 16:43:37 -0800

    Add Enroll component

---
+++ src/Components/home/promotion/Enroll.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Fade } from 'react-reveal';
+
+class Enroll extends Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      formError: false,
+      formSuccess: '',
+      formData: {
+        email: {
+          element: 'input',
+          value: '',
+          config: {
+            name: 'email',
+            type: 'email',
+            placeholder: "Enter Your Email"
+          },
+          validation: {
+            required: true,
+            email: true
+          },
+          valid: false,
+          validationMessage: ''
+        }
+      }
+    };
+  }
+  submitForm(){
+
+  }
+  render() {
+    return (
+      <Fade>
+        <div className="enroll_wrapper">
+          <form onSubmit={(e) => this.submitForm(e)}>
+            <div className="enroll_title">
+              Enter your email
+            </div>
+            <div className="enroll_input">
+              
+            </div>
+          </form>
+        </div>
+      </Fade>
+    )
+  }
+};
+
+export default  Enroll;
--- src/Components/home/promotion/index.js
+++ src/Components/home/promotion/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PromotionAnimation from './PromotionAnimation.js';
+import Enroll from './Enroll.js';
 
 const Promotion = () => {
   return (
     <div className="promotion_wrapper" style={{background: "#fff"}}>
       <div className="container">
         <PromotionAnimation /> 
+        <Enroll />
       </div>
     </div>
   )
