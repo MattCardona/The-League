64c6e22166555858d6f74304865bd160eba596a2
Matt <boblikesbbq@gmail.com>
2019-01-03 15:24:09 -0800

    Add auth PublicRoutes component

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
+
 import Layout from './Hoc/Layout.js';
-import { Switch, Route } from 'react-router-dom';
 import Home from './Components/home/index.js';
 import SignIn from './Components/signin/index.js';
 import Dashboard from './Components/admin/Dashboard.js';
 import PrivateRoutes from './Components/authRoutes/PrivateRoutes.js';
+import PublicRoutes from './Components/authRoutes/PublicRoutes.js';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
          <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
-         {/* <Route exact component={Dashboard} path="/dashboard" /> */}
-         <Route exact component={Home} path="/" />
-         <Route exact component={SignIn} path="/sign_in" />
+         <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
+         <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={SignIn} />
       </Switch>
     </Layout>
   )
