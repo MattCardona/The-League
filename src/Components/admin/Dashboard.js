e27e127608e4a9e54bc1550d0f1c8e049ffa8109
Matt <boblikesbbq@gmail.com>
2019-01-02 16:43:30 -0800

    Add AdminNav component to AdminLayout

--- src/Components/admin/Dashboard.js
+++ src/Components/admin/Dashboard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import AdminLayout from '../../Hoc/AdminLayout.js';
 
 const Dashboard = () => {
   return (
-    <div className="user_dashboard">
-      <div>
-        This is going to be the dashboard
+    <AdminLayout>
+      <div className="user_dashboard">
+        <div>
+          This is going to be the dashboard
+        </div>
       </div>
-    </div>
+    </AdminLayout>
   )
 };
 
---
+++ src/Components/admin/nav/AdminNav.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';  
+import { ListItem } from '@material-ui/core';
+
+const AdminNav = () => {
+  const links = [
+    {
+      title: 'Matches',
+      linkTo: '/admin_matches'
+    },
+    {
+      title: 'Add Match',
+      linkTo: '/admin_matches/edit_match'
+    },
+    {
+      title: 'Players',
+      linkTo: '/admin_players'
+    },
+    {
+      title: 'Add Player',
+      linkTo: '/admin_players/add_player'
+    }
+  ]
+  const styles = {
+    color: '#fff',
+    fontWeight: '300',
+    borderBottom: '1px solid #353535'
+  }
+  const renderItems = () => (
+    links.map((link) => (
+      <Link to={link.linkTo} key={link.title}>
+        <ListItem button style={styles}> 
+          {link.title}
+        </ListItem>
+      </Link>
+    ))
+  )
+  return (
+    <div>
+      {renderItems()}     
+    </div>
+  )
+};
+
+export default AdminNav;
