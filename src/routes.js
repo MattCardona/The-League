6ef2ce89a232336abc8993968ed479d98254c240
Matt <boblikesbbq@gmail.com>
2019-01-02 16:21:23 -0800

    Add intial Dashboard component

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import Layout from './Hoc/Layout.js';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/home/index.js';
 import SignIn from './Components/signin/index.js';
-
+import Dashboard from './Components/admin/Dashboard.js';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+         <Route exact component={Dashboard} path="/dashboard" />
          <Route exact component={Home} path="/" />
          <Route exact component={SignIn} path="/sign_in" />
       </Switch>
