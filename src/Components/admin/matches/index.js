4ae835e90c8ac0191c61de96466dc9050ba99d3c
Matt <boblikesbbq@gmail.com>
2019-01-04 17:11:14 -0800

    Add progress bar

--- src/Components/admin/matches/index.js
+++ src/Components/admin/matches/index.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AdminLayout from '../../../Hoc/AdminLayout.js';
+import { fireMatches, firebase } from '../../../firebase.js';
+import { firebaseLooper, reverseArray } from '../../ui/misc.js';
 
-import {Table, TableBody, TableCell, TableHead, TableRow, Paper} from '@material-ui/core';
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper, CircularProgress } from '@material-ui/core';
+
 // import TableBody from '@material-ui/core/TableBody';
 // import TableCell from '@material-ui/core/TableCell';
 // import TableHead from '@material-ui/core/TableHead';
 // import TableRow from '@material-ui/core/TableRow';
 // import Paper from '@material-ui/core/Paper';
 
-const AdminMatches = () => {
-  return (
-    <AdminLayout>
-      This is where i will place a table      
-    </AdminLayout>
-  )
+class AdminMatches extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      matches: []
+    }
+  }
+  componentDidMount() {
+    fireMatches.once('value').then(snapshot => {
+      const matches = firebaseLooper(snapshot);
+      
+      this.setState(() => ({
+        isLoading: false,
+        matches: reverseArray(matches)
+      }))
+    })
+  }
+  render(){
+    return (
+      <AdminLayout>
+        <div className="admin_progress">
+          {this.state.isLoading ? 
+            <CircularProgress thickness={5.5}
+              style={{color: "#903749"}}
+
+            />
+            : ''
+          }
+        </div>
+      </AdminLayout>
+    )
+  }
 };
 
 export default AdminMatches;
