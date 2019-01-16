c5fe75f3da0cc579dc88d8268ced87ee2b4a7fb4
Matt <boblikesbbq@gmail.com>
2019-01-16 14:56:47 -0800

    Add LeagueTable component

--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import { CircularProgress } from '@material-ui/core';
+import LeagueTable from './table.js';
+import { fireMatches } from '../../firebase.js';
+import { firebaseLooper, reverseArray } from '../ui/misc.js';
+
 
 class TheMatches extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true,
+      matches: [],
+      filterMatches: [],
+      playerFilters: 'All',
+      resultFilter: 'All'
+    };
+  }
+  componentDidMount(){
+    fireMatches.once('value').then((snapshot) => {
+      const matches = firebaseLooper(snapshot);
+      this.setState(() => ({
+        loading: false,
+        matches: reverseArray(matches),
+        filterMatches: reverseArray(matches)
+      }));
+    })
+  }
   render() {
+    const state = this.state;
     return (
-      <div>
-        This the TheMatches
+      <div className="the_matches_container">
+        <div className="the_matches_wrapper">
+          <div className="left">
+            
+          </div>
+
+          <div className="right">
+            <LeagueTable />
+          </div>
+
+        </div>
       </div>
     )
   }
